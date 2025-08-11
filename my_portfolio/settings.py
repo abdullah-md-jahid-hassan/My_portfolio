d3280e51917834099631c732110dca5b4fc3ac91
abdullah-md-jahid-hassan <abdullahmdjahidhassan@gmail.com>
2025-08-12 03:20:33 +0600

    Certification, refarence and other inhancement

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xbtmi1kz)3rufbr)4s!3fs@fi4tlxx@ku2)6*_481av#94e+7&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abdullahmdjahidhassan.pythonanywhere.com']
 
 
 # Application definition
